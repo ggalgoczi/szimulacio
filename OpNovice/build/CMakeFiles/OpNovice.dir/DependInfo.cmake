56a7a000f2a7eddd03838e141765bce9359b91ee
ggalgoczi <galgoczi@caesar.elte.hu>
2017-09-04 09:57:05 +0200

    files added

---
+++ OpNovice/build/CMakeFiles/OpNovice.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/galgoczi/simulation/OpNovice/OpNovice.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/OpNovice.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNoviceActionInitialization.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNoviceActionInitialization.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNoviceDetectorConstruction.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNoviceDetectorConstruction.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNovicePhysicsList.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNovicePhysicsList.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNovicePhysicsListMessenger.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNovicePhysicsListMessenger.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNovicePrimaryGeneratorAction.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNovicePrimaryGeneratorAction.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNovicePrimaryGeneratorMessenger.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNovicePrimaryGeneratorMessenger.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNoviceRunAction.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNoviceRunAction.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNoviceStackingAction.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNoviceStackingAction.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNoviceSteppingAction.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNoviceSteppingAction.cc.o"
+  "/home/galgoczi/simulation/OpNovice/src/OpNoviceSteppingVerbose.cc" "/home/galgoczi/simulation/OpNovice/build/CMakeFiles/OpNovice.dir/src/OpNoviceSteppingVerbose.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/galgoczi/ess/geant/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ScintTutorial/build/CMakeFiles/ScintTutorial.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/galgoczi/simulation/ScintTutorial/OpNovice.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/OpNovice.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNoviceActionInitialization.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNoviceActionInitialization.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNoviceDetectorConstruction.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNoviceDetectorConstruction.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNovicePhysicsList.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNovicePhysicsList.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNovicePhysicsListMessenger.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNovicePhysicsListMessenger.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNovicePrimaryGeneratorAction.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNovicePrimaryGeneratorAction.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNovicePrimaryGeneratorMessenger.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNovicePrimaryGeneratorMessenger.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNoviceRunAction.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNoviceRunAction.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNoviceStackingAction.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNoviceStackingAction.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNoviceSteppingAction.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNoviceSteppingAction.cc.o"
+  "/home/galgoczi/simulation/ScintTutorial/src/OpNoviceSteppingVerbose.cc" "/home/galgoczi/simulation/ScintTutorial/build/CMakeFiles/ScintTutorial.dir/src/OpNoviceSteppingVerbose.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/galgoczi/ess/geant/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
