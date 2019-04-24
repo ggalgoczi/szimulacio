8230aeee0bd965a57deebd0594e0de7104855556
ggalgoczi <galgoczi@caesar.elte.hu>
2019-04-24 21:51:28 +0200

    ok

--- Thickness_Experiment/LXe.cc
+++ Thickness_Experiment/LXe.cc
@@ -67,7 +67,7 @@ int main(int argc, char** argv)
 	
 #ifdef G4MULTITHREADED
   G4MTRunManager * runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(7);
+  runManager->SetNumberOfThreads(3);
 #else
   G4RunManager * runManager = new G4RunManager;
 #endif
