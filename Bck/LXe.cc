5a4cce7ffece684e138ad56b8c45c96a29cfbc83
ggalgoczi <gaborgalgoczi@gmail.com>
2019-07-29 13:33:50 +0200

    aa

--- Bck/LXe.cc
+++ Bck/LXe.cc
@@ -67,7 +67,7 @@ int main(int argc, char** argv)
 	
 #ifdef G4MULTITHREADED
   G4MTRunManager * runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(1);
+  runManager->SetNumberOfThreads(2);
 #else
   G4RunManager * runManager = new G4RunManager;
 #endif
@@ -77,7 +77,7 @@ int main(int argc, char** argv)
   G4int seed = time( NULL ); 
   G4cout << seed << G4endl;
   G4cout << seed << G4endl;
-exit(-1);  
+//exit(-1);  
   G4Random::setTheSeed( seed );
 
 
