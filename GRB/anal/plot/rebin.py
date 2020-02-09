510d092d88fb3415093f74244f73030d1a4d19d1
gaborgalgoczi <gaborgalgoczi@gmail.com>
2020-02-09 21:30:47 +0100

    aa

--- GRB/anal/plot/rebin.py
+++ GRB/anal/plot/rebin.py
@@ -1,5 +1,6 @@
 import os, sys, math
 import numpy as np
+from matplotlib import pyplot as plt
 
 # list of numbers
 a=[]
@@ -7,14 +8,13 @@ f = open("5.res", 'r')
 for line in f:
     a.append(int(line))
     
-a=a[1:]
-print a    
-exit()
+a=a[1:] # Cutting entry for zeros
+#print a    
 # number of entries
 L = int(len(a))
 
 # desired bin width
-W = 3
+W = 30
 
 # number of bins with width W
 N = int(L/W)
@@ -31,4 +31,7 @@ for n in range(0,N):
     a_rebin[n]=[W*n,k]
 
 # print
+plt.plot(a)
+plt.plot(a_rebin)
+plt.show()
 print a_rebin
