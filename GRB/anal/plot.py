729064acfacee31e43eed4642a1b85c7f877c7cb
ggalgoczi <gaborgalgoczi@gmail.com>
2020-02-11 14:14:27 +0100

    added PLOT script

--- GRB/anal/plot.py
+++ GRB/anal/plot.py
@@ -1,23 +1,53 @@
 import os
-dir_path = os.getcwd()
-assert dir_path == "/home/galgoczi/cubesat/Thickness_Experiment/anal"
+import matplotlib.pyplot as plt
+import numpy as np
+import operator
+import pandas as pd
+import math
+degree=[]
+value=[]
+Flux=2.25023 # particle / cm**2 / sec
+AreaOfIrradiation=30*30 # in cm
+ParticlesSimulated=100000
+Norm=Flux*AreaOfIrradiation/ParticlesSimulated
 
-for filename in os.listdir("./"):
-    if filename.endswith(".txt"):
-        os.remove(filename)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel('Direction of the GRB [$^\circ$]')
+ax.set_ylabel('Count rate [cps]')
 
-print("Deleted all files")
+f = open("dir", 'r')
+for line in f:
+	degree.append(float(line.split()[0]))		
+	value.append(float(line.split()[1]))	
+print degree
+print value	
 
-for filename in os.listdir("../res/"):
-    if filename.endswith(".res"): 
-         # print(os.path.join(directory, filename))
-        #print filename.split('.')
-        with open("../res/"+filename) as f:
-			first_line = f.readline()
-			
-			with open(filename.split('.')[1]+".txt", "a") as myfile:
-				myfile.write(filename.split('.')[0]+" "+str(10000-int(first_line))+"\n")
-			
-        continue
-    else:
-        continue
+plt.errorbar(
+            [x * 10 for x in degree],
+            [x * Norm for x in value],
+            yerr=[math.sqrt(x)*Norm for x in value],
+            linestyle="None",
+            label='Count rate with treshold'
+            )
+
+
+degree=[]
+value=[]
+f = open("diir", 'r')
+for line in f:
+	degree.append(float(line.split()[0]))		
+	value.append(float(line.split()[1]))	
+print degree
+print value	
+
+plt.errorbar(
+            [x * 10 for x in degree],
+            [x * Norm for x in value],
+            yerr=[math.sqrt(x)*Norm for x in value],
+            linestyle="None",
+            label='Count rate without treshold'
+            )
+plt.legend(  )
+fig.savefig("grb_direction_detection.png")
+plt.show()
