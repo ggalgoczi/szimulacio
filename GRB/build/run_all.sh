413b74fcdf182e8194e21d8400d0aee9a3404c15
gaborgalgoczi <gaborgalgoczi@gmail.com>
2020-08-01 11:34:27 +0200

    aa

--- GRB/build/run_all.sh
+++ GRB/build/run_all.sh
@@ -1,3 +1,8 @@
-for f in ../macs/*.sh; do  # or wget-*.sh instead of *.sh
+#for f in ../macs/*.sh; do  # or wget-*.sh instead of *.sh
+#declare -a StringArray=("../macs/0.sh" "../macs/1.sh" "../macs/2.sh" "../macs/37.sh" )
+declare -a StringArray=("../macs/36.sh" )
+
+
+for f in ${StringArray[@]}; do  # or wget-*.sh instead of *.sh
   bash "$f" -H   || break # if needed 
 done
